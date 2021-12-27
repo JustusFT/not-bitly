1aa6a226fb25d087759231b75840eac43ea57b1a
Justus Tumacder <justusft@gmail.com>
2021-12-27 12:22:49 +0800

    Fix create()'s http check

--- client/src/util/api/linksApi.js
+++ client/src/util/api/linksApi.js
@@ -1,25 +1,25 @@
 function create(url) {
   // prepend an http:// if not provided
-  if (!/$https?:\/\//.test(url)) {
+  if (!/^https?:\/\//.test(url)) {
     url = `http://${url}`;
   }
 
   return fetch(`/api/links`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({ url })
+    body: JSON.stringify({ url }),
   });
 }
 
 function destroy(id) {
   return fetch(`/api/links/${id}`, {
-    method: 'DELETE'
+    method: "DELETE",
   });
 }
 
 export default {
   create,
-  destroy
+  destroy,
 };
