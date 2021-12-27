5c0ebb8924e1a0d5aec8bea30901f993fa71667d
Justus Tumacder <justusft@gmail.com>
2021-12-27 12:26:25 +0800

    Use SSL for postgres

--- knexfile.js
+++ knexfile.js
@@ -2,30 +2,33 @@
 
 module.exports = {
   development: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'not_bitly_development',
+      database: "not_bitly_development",
       user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    client: "postgresql",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      tableName: "knex_migrations",
+    },
+  },
 };
